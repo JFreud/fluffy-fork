9117d923612afe18127f5849af000c68e783f5ff
JFreud <jrm.frdnbrg@gmail.com>
2017-11-18 18:56:20 -0500

    fixed hw

--- fork.c
+++ fork.c
@@ -2,32 +2,37 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <time.h>
+#include <sys/types.h>
 
 
 int child() {
-  unsigned int therand = rand() % 15 + 5;
-  printf("I, the child, have a pid of %d\n", getpid());
-  sleep(therand);
+  srand(time(NULL) + getpid());
+  unsigned int time_asleep = rand() % 15 + 5;
+  printf("I, the child, have a pid of %d and will sleep for %d seconds!\n", getpid(), time_asleep);
+  sleep(time_asleep);
   printf("I, the child with a pid of %d, have awoken and finished!\n", getpid());
-  return(therand);
+  exit(time_asleep);
 }
 
 int main() {
 
   int status;
-  srand(time(NULL));
+
   printf("=======pre-fork=======\n");
   int f = fork();
+  int f2;
 
-  if (f) {
-    f = fork();
-    int child_pid = wait(&status);
-    printf("The finished child:\npid: %d\t time asleep: %d\n", child_pid, WEXITSTATUS(status));
-    printf("I, the parent, am done\n");
-  }
-  else {
+  if (f) {f2 = fork();}
+  else {f2 = 1;}
+
+
+  if (!f || !f2) {
     child();
   }
+
+  int child_pid = wait(&status);
+  printf("The finished child:\npid: %d\t time asleep: %d\n", child_pid, WEXITSTATUS(status));
+  printf("I, the parent, am done\n");
 
   return 0;
 }
